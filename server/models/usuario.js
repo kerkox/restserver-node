5c1655367e23f66b506d87f06254a7ed830cabdb
Paul Cortes <paulkerg@gmail.com>
2020-07-26 20:41:42 -0500

    agregado el paquete de mongoose unique validator para poder hacer los mensajes de error de tipo unicos mas claros

--- server/models/usuario.js
+++ server/models/usuario.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const uniqueValidator = require('mongoose-unique-validator');
 let Schema = mongoose.Schema;
 
 let usuarioSchema = new Schema({
@@ -33,5 +33,7 @@ let usuarioSchema = new Schema({
     default: false
   }
 });
+
+usuarioSchema.plugin(uniqueValidator)
 
 module.exports = mongoose.model('Usuario', usuarioSchema)
