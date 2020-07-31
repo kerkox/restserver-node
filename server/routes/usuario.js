19bdecd6e64d5ff890b9d4f71e474e71c0130cce
Paul Cortes <paulkerg@gmail.com>
2020-07-30 22:16:46 -0500

    Conteo de usuarios para retornalos en la respuesta GET

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -22,9 +22,12 @@ app.get("/usuario", function (req, res) {
         });
       }
 
-      res.json({
-        ok: true,
-        usuarios,
+      Usuario.count({}, (err, conteo) => {
+        res.json({
+          ok: true,
+          usuarios,
+          cuantos: conteo,
+        });
       });
     });
 });
