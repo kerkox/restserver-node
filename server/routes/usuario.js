60cf9fb05d442e26aa26f3880ed26a4d3edc058d
Paul Cortes <paulkerg@gmail.com>
2020-07-30 21:58:18 -0500

    Implementacion de codigo para actualizar

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,52 +1,66 @@
-const express = require('express')
-const bcrypt = require('bcrypt')
-const Usuario = require('../models/usuario');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const Usuario = require("../models/usuario");
 
-const app = express()
+const app = express();
 
+app.get("/usuario", function (req, res) {
+  res.json("get Usuario");
+});
 
-app.get('/usuario', function(req,res) {
-  res.json('get Usuario')
-})
-
-app.post('/usuario', function(req,res) {
+app.post("/usuario", function (req, res) {
   let body = req.body;
   let usuario = new Usuario({
     nombre: body.nombre,
     email: body.email,
     password: bcrypt.hashSync(body.password, 10),
-    role: body.role
+    role: body.role,
   });
 
-  usuario.save( (err, usuarioDB) => {
-    if(err) {
+  usuario.save((err, usuarioDB) => {
+    if (err) {
       return res.status(400).json({
         ok: false,
-        err
-      })
+        err,
+      });
     }
 
     res.json({
-      ok:true,
-      usuario:usuarioDB
-    })
+      ok: true,
+      usuario: usuarioDB,
+    });
   });
- 
-})
+});
 
-app.put('/usuario/:id', function(req,res) {
-  let id = req.params.id
+app.put("/usuario/:id", function (req, res) {
+  let id = req.params.id;
+  let body = req.body;
+
+  Usuario.findByIdAndUpdate(
+    id,
+    body,
+    { new: true, runValidators: true },
+    (err, usuarioDB) => {
+      if (err) {
+        return res.status(400).json({
+          ok: false,
+          err,
+        });
+      }
+
+      res.json({
+        ok: true,
+        usuario: usuarioDB,
+      });
+    }
+  );
+});
+
+app.delete("/usuario/:id", function (req, res) {
+  let id = req.params.id;
   res.json({
-    id
-  })
-})
+    id,
+  });
+});
 
-app.delete('/usuario/:id', function(req,res) {
-  let id = req.params.id
-  res.json({
-    id
-  })
-})
-
-
-module.exports = app
+module.exports = app;
