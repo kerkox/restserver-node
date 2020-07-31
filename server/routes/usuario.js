befd7e68269ca9cc58a8181eb76078bb28212f70
Paul Cortes <paulkerg@gmail.com>
2020-07-30 22:07:18 -0500

    Implementada libreria Underscorejs para las validaciones en el PUT de usuario

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const _ = require("underscore");
 const Usuario = require("../models/usuario");
 
 const app = express();
@@ -34,7 +35,7 @@ app.post("/usuario", function (req, res) {
 
 app.put("/usuario/:id", function (req, res) {
   let id = req.params.id;
-  let body = req.body;
+  let body = _.pick(req.body, ["nombre", "email", "img", "role", "estado"]);
 
   Usuario.findByIdAndUpdate(
     id,
