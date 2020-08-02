6aea8f8328592c5691bdf143686a70c3f4af9ac2
Paul Cortes <paulkerg@gmail.com>
2020-08-02 16:08:06 -0500

    ajustes para version de produccion y desarrollo

--- server/server.js
+++ server/server.js
@@ -15,7 +15,9 @@ app.use(bodyParser.json())
 app.use( require('./routes/usuario'))
 
 
-mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+mongoose.connect(process.env.URLDB, 
+  { useNewUrlParser: true, useCreateIndex: true},
+  (err, res) => {
   if(err) throw new err;
   console.log("Base de datos ONLINE");
 })
@@ -23,4 +25,5 @@ mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
 app.listen(process.env.PORT, () => {
   console.log("Escuchando el puerto 3000");
 })
+
 
