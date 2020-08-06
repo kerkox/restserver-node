47ec98f298f19e5e7b2028454dea4b92c17ae196
Paul Cortes <paulkerg@gmail.com>
2020-08-05 20:22:08 -0500

    Implementacion de middleware para uso de Token JWT

---
+++ server/middlewares/autenticacion.js
@@ -0,0 +1,27 @@
+// ======================
+// Verificar Token
+
+const jwt = require("jsonwebtoken");
+
+// ======================
+
+let verificaToken = (req, res, next) => {
+  let token = req.get('Authorization');
+  jwt.verify(token,process.env.SEED, (err, decoded)=> {
+    if ( err ){
+      return res.status(401).json({
+        ok: false,
+        err: {
+          message: 'Token no válido'
+        }
+      })
+    }
+    req.usuario = decoded.usuario
+    next();
+  })
+}
+
+
+module.exports = {
+  verificaToken
+}
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -2,10 +2,12 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const _ = require("underscore");
 const Usuario = require("../models/usuario");
+const { verificaToken } = require("../middlewares/autenticacion");
 
 const app = express();
 
-app.get("/usuario", function (req, res) {
+app.get("/usuario", verificaToken, function (req, res) {
+  
   let from = req.query.from || 0;
   from = Number(from);
 
@@ -33,7 +35,7 @@ app.get("/usuario", function (req, res) {
     });
 });
 
-app.post("/usuario", function (req, res) {
+app.post("/usuario", verificaToken,function (req, res) {
   let body = req.body;
   let usuario = new Usuario({
     nombre: body.nombre,
@@ -57,7 +59,7 @@ app.post("/usuario", function (req, res) {
   });
 });
 
-app.put("/usuario/:id", function (req, res) {
+app.put("/usuario/:id",verificaToken, function (req, res) {
   let id = req.params.id;
   let body = _.pick(req.body, ["nombre", "email", "img", "role", "estado"]);
 
@@ -81,7 +83,7 @@ app.put("/usuario/:id", function (req, res) {
   );
 });
 
-app.delete("/usuario/:id", function (req, res) {
+app.delete("/usuario/:id",verificaToken, function (req, res) {
   let id = req.params.id;
 
   let _delete = { estado: false, deleted_at: new Date() };
