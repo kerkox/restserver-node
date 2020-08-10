e24a0f9058f37a99c36ed95777457aa0e103a4af
Paul Cortes <paulkerg@gmail.com>
2020-08-09 21:42:53 -0500

    implementacion de nuevas variables de CLIENT_ID de google en el backend

--- server/config/config.js
+++ server/config/config.js
@@ -30,3 +30,9 @@ if( process.env.NODE_ENV == 'dev') {
 }
 
 process.env.URLDB = urlDB
+
+
+// ========================
+// Google Client ID
+// ========================
+process.env.CLIENT_ID = process.env.CLIENT_ID || '792275556301-o3r53cvf15re27q3ej1bc0il5269toed.apps.googleusercontent.com'
--- server/routes/login.js
+++ server/routes/login.js
@@ -1,6 +1,11 @@
 const express = require("express");
+
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(process.env.CLIENT_ID);
+
 const Usuario = require("../models/usuario");
 const app = express();
 app.post("/login", (req,res) => {
@@ -39,8 +44,23 @@ app.post("/login", (req,res) => {
   
 })
 
+//Configuraciones de google
+async function verify(token) {
+  const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      // Or, if multiple clients access the backend:
+      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  const payload = ticket.getPayload();  
+}
+
 app.post('/google', (req,res)=> {
   let token = req.body.idtoken;
+  verify(token)
+  res.json({
+    token
+  })
 })
 
 module.exports = app;
