2d30a17b3a832c25f1583cac4a765614ba75113d
Paul Cortes <paulkerg@gmail.com>
2020-08-17 21:10:48 -0500

    Mostrar imagenes de forma protegida

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -1,4 +1,3 @@
-
 const jwt = require("jsonwebtoken");
 
 // ======================
@@ -7,8 +6,8 @@ const jwt = require("jsonwebtoken");
 
 let verificaToken = (req, res, next) => {
   let token = req.get('Authorization');
-  jwt.verify(token,process.env.SEED, (err, decoded)=> {
-    if ( err ){
+  jwt.verify(token, process.env.SEED, (err, decoded) => {
+    if (err) {
       return res.status(401).json({
         ok: false,
         err: {
@@ -27,8 +26,8 @@ let verificaToken = (req, res, next) => {
 let verificaAdmin_Role = (req, res, next) => {
   let usuario = req.usuario;
 
-  if(usuario.role === 'ADMIN_ROLE'){
-    next();  
+  if (usuario.role === 'ADMIN_ROLE') {
+    next();
   } else {
     return res.json({
       ok: false,
@@ -38,11 +37,30 @@ let verificaAdmin_Role = (req, res, next) => {
     })
   }
 
-  
+
+}
+// ========================================
+// Verifica token para imagen
+// ========================================
+let verificaTokenImg = (req, res, next) => {
+  let token = req.query.token;
+  jwt.verify(token, process.env.SEED, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({
+        ok: false,
+        err: {
+          message: 'Token no válido'
+        }
+      })
+    }
+    req.usuario = decoded.usuario
+    next();
+  })
 }
 
 
 module.exports = {
   verificaToken,
-  verificaAdmin_Role
+  verificaAdmin_Role,
+  verificaTokenImg
 }
